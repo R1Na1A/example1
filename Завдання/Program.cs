85de8c7ad0a5ea81033a27c0a1fced2e3645fd0b
ASUS ROG <ASUS ROG@ASUS>
2023-05-30 16:22:12 +0300

    Add project files.

---
+++ Завдання/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+
+namespace Lab4
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            string sentence = "Alfred bought the apples but Alfred forgot that Alfred sold the apples";
+
+            string[] words = sentence.Split(' ');
+
+
+
+            Console.WriteLine(sentence);
+
+            Console.WriteLine("\nWords that appear only once:");
+
+
+
+            foreach (string word in words)
+
+            {
+
+                if (sentence.IndexOf(word) == sentence.LastIndexOf(word))
+
+                {
+
+                    Console.WriteLine("-" + word);
+
+                }
+
+            }
+
+            Console.WriteLine();
+
+        }
+
+    }
+
+} // See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
